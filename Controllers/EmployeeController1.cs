fefbe8c67b836fac1a5f71fe521db4055449f6fc
omar2512 <113863685+omar2512@users.noreply.github.com>
2022-11-20 19:43:27 +0200

    Add files via upload

---
+++ Controllers/EmployeeController1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CrudOperation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperation.Controllers
+{
+    public class EmployeeController1 : Controller
+    {
+        public EmployeeDBcontext dbcontext;
+        public EmployeeController1(EmployeeDBcontext employeeDBcontext)
+        {
+            dbcontext = employeeDBcontext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employelist =await dbcontext.Employees.ToListAsync();
+            return View(employelist);   
+        }
+        [HttpGet]
+        public IActionResult ADD()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ADD(Employee empoye)
+        {
+            await dbcontext.Employees.AddAsync(empoye);
+            await dbcontext.SaveChangesAsync();
+            return RedirectToAction("ADD");
+        }
+        [HttpGet]
+        public async Task<IActionResult> View(int? id)
+        {
+            var employe =await dbcontext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+           
+                return await Task.Run(()=> View("View",employe));
+            
+           
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> View(Employee employss)
+        {
+
+
+            dbcontext.Update(employss);
+            await dbcontext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Employee employss)
+        {
+            dbcontext.Remove(employss);
+            await dbcontext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+    }

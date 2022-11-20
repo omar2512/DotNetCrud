687479b836d92fc231e2b9668b7a5683837bf35b
omar2512 <113863685+omar2512@users.noreply.github.com>
2022-11-20 19:48:55 +0200

    Add files via upload
    Crud operation project using .net core v6 and sql server 2019

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudOperation.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public float Salary { get; set; }
+        public DateTime DataOfBirth { get; set; }
+        public string? Department { get; set; }
+
+    }
+}
---
+++ Models/EmployeeDBcontext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CrudOperation.Models
+{
+    public class EmployeeDBcontext:DbContext
+    {
+        public EmployeeDBcontext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Employee>? Employees { get; set; }
+    }
+}
